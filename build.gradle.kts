f08ea8c4a5cb790e54f7beccc0d81a484f754f63
Paolo Conte <paolo.conte@gmail.com>
2024-04-01 11:19:28 +0200

    publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     kotlin("multiplatform")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 group = "io.paoloconte"
@@ -10,6 +14,46 @@ version = "1.0"
 repositories {
     mavenCentral()
 }
+
+mavenPublishing {
+    configure(KotlinMultiplatform(
+        javadocJar = JavadocJar.Empty(),
+        sourcesJar = true,
+    ))
+
+    coordinates(project.group.toString(), rootProject.name, project.version.toString())
+
+    pom {
+        name.set("Kotlin ItsOk library")
+        description.set("A little library that provides an improved way to use Result in Kotlin")
+        inceptionYear.set("2024")
+        url.set("https://github.com/PaoloConte/kotlin-itsok")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/PaoloConte/kotlin-itsok/blob/main/LICENSE")
+                distribution.set("https://github.com/PaoloConte/kotlin-itsok/blob/main/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("PaoloConte")
+                name.set("Paolo Conte")
+                url.set("https://github.com/PaoloConte/")
+            }
+        }
+        scm {
+            url.set("https://github.com/PaoloConte/kotlin-itsok")
+            connection.set("scm:git:git@github.com:PaoloConte/kotlin-itsok.git")
+            developerConnection.set("scm:git:ssh://git@github.com/PaoloConte/kotlin-itsok.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+}
+
 
 kotlin {
     jvmToolchain(11)
